776b1d02e60eb008f78427bf644d2c315d030d51
Darlene Julien <darlenejulien@darlenes-mbp.ef.com>
2020-01-15 09:55:03 -0500

    added userlogin and password

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -2,15 +2,20 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
   items: forwardTo('db'),
-  items: forwardTo('db'),
+  item: forwardTo('db'),
   itemsConnection: forwardTo('db'),
-
-
-//   async items(parent, args, ctx, info) {
-//   //   console.log('Getting Items!!');
-//     const items = await ctx.db.query.items();
-//     return items;
-//   },
+  me(parent, args, ctx, info) {
+    // check if there is a current user ID
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Query;
